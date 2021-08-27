4c0392bc7d1b8b64f583d1a2903e8d58ccf6c71a
Hanif Ahmed <hanifahmed@hotmail.com>
2021-08-28 00:36:40 +0100

    firstFormCompl

--- lib/screens/adddetails.dart
+++ lib/screens/adddetails.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_conn/screens/getdocs.dart';
 import 'package:flutter/material.dart';
-
-import '../alldata.dart';
 
 class AddDetails extends StatefulWidget {
   const AddDetails({Key? key}) : super(key: key);
@@ -14,82 +13,144 @@ class _AddDetailsState extends State<AddDetails> {
   TextEditingController name = new TextEditingController();
   TextEditingController surname = new TextEditingController();
   TextEditingController postcode = new TextEditingController();
+  TextEditingController email = new TextEditingController();
+  TextEditingController business_name = new TextEditingController();
+  TextEditingController business_desc = new TextEditingController();
+  TextEditingController service_radius = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
+    return
+        // SingleChildScrollView(
+        //   child:
+        Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text("Add Details"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(40.0),
-        child: Center(
-          child: Column(
-            children: [
-              TextFormField(
-                controller: name,
-                decoration: InputDecoration(hintText: "name"),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextFormField(
-                controller: surname,
-                decoration: InputDecoration(hintText: "surname"),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextFormField(
-                controller: postcode,
-                decoration: InputDecoration(hintText: "postcode"),
-              ),
-              SizedBox(height: 10.09),
-              TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.red, // foreground
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(40.0),
+          child: Center(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: name,
+                  decoration: InputDecoration(hintText: "name"),
                 ),
-                onPressed: () {
-                  Map<String, dynamic> data = {
-                    "field1": name.text,
-                    "field2": surname.text,
-                    "field3": postcode.text
-                  };
-                  FirebaseFirestore.instance.collection("newc").add(data);
-                },
-                child: Text("Submit"),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.red, // foreground
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: surname,
+                  decoration: InputDecoration(hintText: "surname"),
                 ),
-                onPressed: () async {
-                  DocumentSnapshot variable = await FirebaseFirestore.instance
-                      .collection('newc')
-                      .doc('BsZe6YmjYjsWTbCLtgP8')
-                      .get();
-                  print(variable['field1']);
-                  print(variable['field2']);
-                  print(variable['field2']);
-                },
-                child: Text("Get Data"),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.red, // foreground
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: postcode,
+                  decoration: InputDecoration(hintText: "postcode"),
                 ),
-                onPressed: () async {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => alldata()));
-                },
-                child: Text("Get All Data"),
-              ),
-            ],
+                SizedBox(height: 10.09),
+                TextFormField(
+                  controller: email,
+                  decoration: InputDecoration(hintText: "email"),
+                ),
+                SizedBox(height: 10.09),
+                TextFormField(
+                  controller: business_name,
+                  decoration: InputDecoration(hintText: "business name"),
+                ),
+                SizedBox(height: 10.09),
+                TextFormField(
+                  controller: business_desc,
+                  decoration: InputDecoration(hintText: "business desc"),
+                ),
+                SizedBox(height: 10.09),
+                TextFormField(
+                  controller: service_radius,
+                  decoration: InputDecoration(hintText: "service radius"),
+                ),
+                SizedBox(height: 10.09),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.red, // foreground
+                  ),
+                  onPressed: () {
+                    Map<String, dynamic> data = {
+                      // "f01name": name.text,
+                      // "f02surname": surname.text,
+                      // "f03postcode": postcode.text,
+                      // "f04business_name": business_name.text,
+                      // "f05business_desc": business_desc.text,
+                      // "f06service_radius": service_radius.text
+                      "f01name": name.text,
+                      "f02surname": surname.text,
+                      "f03postcode": postcode.text,
+                      "f04business_name": business_name.text,
+                      "f05business_desc": business_desc.text,
+                      "f06service_radius": service_radius.text,
+                      "f07email": email.text
+                    };
+                    FirebaseFirestore.instance
+                        .collection("new_biz_add")
+                        .add(data);
+                  },
+                  child: Text("Submit"),
+                ),
+
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.red, // foreground
+                  ),
+                  onPressed: () async {
+                    DocumentSnapshot variable = await FirebaseFirestore.instance
+                        .collection('newc')
+                        .doc('7668IAEwXzIZ56arbn70')
+                        .get();
+
+                    print(variable['field1']);
+                    print(variable['field2']);
+                    print(variable['field3']);
+                  },
+                  child: Text("Get Data"),
+                ),
+                //             TextButton(
+                //               style: TextButton.styleFrom(
+                //                 primary: Colors.red, // foreground
+                //               ),
+                //               onPressed: () async {
+                //                 QuerySnapshot snap = await
+                //   FirebaseFirestore.instance.collection('collection').get();
+
+                //   print(document.documentID);
+                // });
+
+                // QuerySnapshot variable = await FirebaseFirestore.instance
+                //     .collection('newc')
+                //     .where('field1', isGreaterThan: 1)
+                //     .get();
+
+                // print(variable["field1"]);
+                // print(variable['field2']);
+                // print(variable['field3']);
+                //   },
+                //   child: Text("GetSearch Data"),
+                // ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.red, // foreground
+                  ),
+                  onPressed: () async {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => GetData()));
+                  },
+                  child: Text("Get All Your Datas"),
+                ),
+              ],
+            ),
           ),
         ),
+        // ),
       ),
     );
   }
---
+++ lib/screens/getdocs.dart
@@ -0,0 +1,44 @@
+//   Script from:  Mins 7:35 @  https://www.youtube.com/watch?v=QshpV3gh708
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class GetData extends StatefulWidget {
+  const GetData({Key? key}) : super(key: key);
+
+  @override
+  _GetDataState createState() => _GetDataState();
+}
+
+class _GetDataState extends State<GetData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Get Data"),
+      ),
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('new_biz_add').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Text('No Value');
+          }
+// 'return script below from bottom sections:
+// https://stackoverflow.com/questions/67564852/flutter-firestore-i-have-a-streambuilder-that-returns-a-certain-field-from-al
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot course = snapshot.data!.docs[index];
+                return ListTile(
+                  title: Text(
+                    course['f04business_name'],
+                  ),
+                  subtitle: Text(course['f05business_desc']),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
